9bd02a0bd60eb66469fd6019f9e957088cbc3dcf
Victor Lellis <vmlellis@gmail.com>
2018-06-17 22:59:34 -0300

    Update incrementCounter

--- js/app.js
+++ js/app.js
@@ -26,11 +26,12 @@ var Cat = function() {
 }
 
 var ViewModel = function() {
+  var self = this;
 
   this.currentCat = ko.observable( new Cat() );
 
   this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
   }
 }
 
